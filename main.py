69643bf74d24dee4b41126d316b7ad2f882e08d2
Abolfazl-the-jesus <yzdanpnahabwalfdl7@gmail.com>
2025-07-10 00:12:43 +0330

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("bot")
+import asyncio
+from aiogram import Bot, dispatcher
+from bot.config import get_setting
+from bot.handlers import start
+
+async def main():
+    setting = get_setting()
+    bot = Bot(token=setting.bot_token)
+    dp = dispatcher()
+
+    dp.include_router(start.router)
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())    
