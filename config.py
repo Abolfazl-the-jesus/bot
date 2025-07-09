dfc583522dc46218c21ff2d580639e8eb2a6da6a
Abolfazl-the-jesus <yzdanpnahabwalfdl7@gmail.com>
2025-07-09 23:36:52 +0330

    config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
-print("config")
+from dataclasses import dataclass
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+@dataclass
+class setting():
+    bot_token: str
+
+
+def get_setting():
+    return setting(bot_token=os.getenv("BOT_TOKEN"))
